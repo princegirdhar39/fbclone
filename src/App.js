6accd9d86b137426c971ca9c06cbaeb8576d838a
Prince Girdhar <princegirdhar1999@gmail.com>
2021-05-13 07:38:51 +0530

    login-page-ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-import Sidebar from './Sidebar'
+import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
+import Login from "./Login";
 
 function App() {
+  const User = null;
   return (
     <div className="app">
+      {!User ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-      <Header />
-
-      
-      <div className="app_body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-
-
-
-      </div>
-      
+          <div className="app_body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
